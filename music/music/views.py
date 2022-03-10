3f0dc9f6e1d12ee176487003bdf1c562c837c33c
anandjaiswal1309 <anandjaiswalanand1024@gmail.com>
2022-03-10 21:54:14 +0530

    Comment

---
+++ music/music/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from musicbeats.models import Song,Watchlater
+from django.db.models import Case, When
+
+# Hi
+def index(request):
+    song = Song.objects.all()[0:3]
+    if request.user.is_authenticated:
+        wl = Watchlater.objects.filter(user=request.user)
+        ids  =[]
+        for i in wl:
+            ids.append(i.video_id)
+        preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(ids)])
+        watch = Song.objects.filter(song_id__in=ids).order_by(preserved)    
+        watch = reversed(watch)
+    else:
+        watch = Song.objects.all()[0:3]    
+    return render(request,'index.html', {'song':song,'watch':watch} )
+
